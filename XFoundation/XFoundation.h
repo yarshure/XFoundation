d18280a8b241723c2fc74ac5c567e1743d66a59d
yarshure <yarshure@gmail.com>
2018-01-05 09:47:29 +0800

    Initial Commit

---
+++ XFoundation/XFoundation.h
@@ -0,0 +1,19 @@
+//
+//  XFoundation.h
+//  XFoundation
+//
+//  Created by yarshure on 2018/1/5.
+//  Copyright © 2018年 yarshure. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XFoundation.
+FOUNDATION_EXPORT double XFoundationVersionNumber;
+
+//! Project version string for XFoundation.
+FOUNDATION_EXPORT const unsigned char XFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XFoundation/PublicHeader.h>
+
+
