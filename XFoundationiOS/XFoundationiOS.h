8af2e42f331abf234eda81a77133dc9b4ac6e5d6
yarshure <yarshure@gmail.com>
2018-01-07 01:20:07 +0800

    shared scheme

---
+++ XFoundationiOS/XFoundationiOS.h
@@ -0,0 +1,19 @@
+//
+//  XFoundationiOS.h
+//  XFoundationiOS
+//
+//  Created by yarshure on 2018/1/7.
+//  Copyright © 2018年 yarshure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XFoundationiOS.
+FOUNDATION_EXPORT double XFoundationiOSVersionNumber;
+
+//! Project version string for XFoundationiOS.
+FOUNDATION_EXPORT const unsigned char XFoundationiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XFoundationiOS/PublicHeader.h>
+
+
